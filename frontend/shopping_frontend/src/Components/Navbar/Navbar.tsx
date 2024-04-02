8ed9512c6b3c582b513c6df587ce34dc18a5d222
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2024-04-03 00:05:13 +0200

    feat:navbar done

---
+++ frontend/shopping_frontend/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { Link } from 'react-router-dom';
+
+const buttons = [
+  <Link to="/sign-in" style={{ textDecoration: 'none', color: 'inherit' }}>
+    <Button key="sign-in" style={{ width: '100px' }}>
+      Login
+    </Button>
+  </Link>,
+  <Link to="/sign-up" style={{ textDecoration: 'none', color: 'inherit' }}>
+    <Button key="sign-up" style={{ width: '100px' }}>
+      Register
+    </Button>
+  </Link>,
+];
+
+export default function NavBar() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        '& > *': {
+          m: 1,
+        },
+      }}
+    >
+      <ButtonGroup
+        size="medium"
+        aria-label="Large button group"
+        style={{ marginRight: '15px', marginTop: '15px' }}
+      >
+        {buttons}
+      </ButtonGroup>
+    </Box>
+  );
+}
--- frontend/shopping_frontend/src/Pages/Home/Home.tsx
+++ frontend/shopping_frontend/src/Pages/Home/Home.tsx
@@ -1,37 +1,50 @@
 import BasicCard from '../../Components/Card/Card';
 import hamburgerImagePath from '../../assets/hamburger-494706_640.jpg';
 import deliveryRiderPath from '../../assets/Free-delivery-vector-royalty-free-PNG.png';
+import NavBar from '../../Components/Navbar/Navbar';
+
 const Home = () => {
   return (
     <div
-      id="card-container"
+      id="home-container"
       style={{
-        display: 'flex',
         width: '100%',
         height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
       }}
     >
+      <div id="navbar-container" style={{ height: '15%' }}>
+        <NavBar />
+      </div>
       <div
+        id="card-container"
         style={{
           display: 'flex',
-          flexDirection: 'row',
+          width: '100%',
+          height: '85%',
           justifyContent: 'center',
           alignItems: 'center',
-          gap: '40px',
         }}
       >
-        <BasicCard
-          reference={'/products'}
-          title={'Products'}
-          image={hamburgerImagePath}
-        />
-        <BasicCard
-          reference={'/orders'}
-          title={'Orders'}
-          image={deliveryRiderPath}
-        />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '40px',
+          }}
+        >
+          <BasicCard
+            reference={'/products'}
+            title={'Products'}
+            image={hamburgerImagePath}
+          />
+          <BasicCard
+            reference={'/orders'}
+            title={'Orders'}
+            image={deliveryRiderPath}
+          />
+        </div>
       </div>
     </div>
   );
